0c89287728fc8c49c097a44258e33ff3ecc40f88
Igor Chernakov <igor.chernakov@gmail.com>
2026-09-09 22:31:46 +0200

    Refactoring

--- build/standalone/render.d
+++
@@ -1,5 +0,0 @@
-/Users/igor/Developer/orca/samples/Book/build/standalone/render.o: \
-  render.c render.h vendor/stb_image.h vendor/stb_truetype.h
-render.h:
-vendor/stb_image.h:
-vendor/stb_truetype.h:
